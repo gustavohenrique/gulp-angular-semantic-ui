6d41f4cf097f9ba565eb26f9dddf1742508b50b0
Gustavo Henrique <gustavo@gustavohenrique.net>
2014-12-23 11:11:42 -0200

    fixed tests

--- test/gulp_file.js
+++ test/gulp_file.js
@@ -7,83 +7,83 @@ var path = require('path');
 var assert = require('assert');
 var helpers = require('yeoman-generator').test;
 
-describe('Gulp Webapp generator: tasks', function () {
-  beforeEach(function (done) {
-    helpers.testDirectory(path.join(__dirname, 'gulp-file'), function (err) {
-      if (err) {
-        return done(err);
-      }
+describe('gulp-angular-semantic-ui generator: tasks', function() {
+    beforeEach(function(done) {
+        helpers.testDirectory(path.join(__dirname, 'gulp-file'), function(err) {
+            if (err) {
+                return done(err);
+            }
 
-      this.webapp = helpers.createGenerator('gulp-angular-semantic-ui:app', [
-        '../../app', [
-          helpers.createDummyGenerator(),
-          'mocha:app'
-        ]
-      ]);
-      this.webapp.options['skip-install'] = true;
+            this.webapp = helpers.createGenerator('gulp-angular-semantic-ui:app', [
+                '../../app', [
+                    helpers.createDummyGenerator(),
+                    'mocha:app'
+                ]
+            ]);
+            this.webapp.options['skip-install'] = true;
 
-      done();
-    }.bind(this));
-  });
-
-  var assertTaskExists = function (generator, taskName, features, done) {
-    helpers.mockPrompt(generator, {
-      features: features
+            done();
+        }.bind(this));
     });
 
-    generator.run({}, function () {
-      var gulpFile = fs.readFileSync('gulpfile.js', 'utf8');
-      var regexGulp = new RegExp('gulp.task\\(\'' + taskName + '\'');
+    var assertTaskExists = function(generator, taskName, features, done) {
+        helpers.mockPrompt(generator, {
+            features: features
+        });
 
-      assert.ok(
-        regexGulp.test(gulpFile),
-        'gulpfile.js does not contain ' + taskName + ' task'
-      );
-      done();
+        generator.run({}, function() {
+            var gulpFile = fs.readFileSync('gulpfile.js', 'utf8');
+            var regexGulp = new RegExp('gulp.task\\(\'' + taskName + '\'');
+
+            assert.ok(
+                regexGulp.test(gulpFile),
+                'gulpfile.js does not contain ' + taskName + ' task'
+            );
+            done();
+        });
+    };
+
+    it('should contain styles task', function(done) {
+        assertTaskExists(this.webapp, 'styles', [], done);
     });
-  };
 
-  it('should contain styles task without Sass included', function (done) {
-    assertTaskExists(this.webapp, 'styles', [], done);
-  });
+    it('should contain jshint task', function(done) {
+        assertTaskExists(this.webapp, 'jshint', [], done);
+    });
 
-  it('should contain jshint task', function (done) {
-    assertTaskExists(this.webapp, 'jshint', [], done);
-  });
+    it('should contain html task', function(done) {
+        assertTaskExists(this.webapp, 'html', [], done);
+    });
 
-  it('should contain html task', function (done) {
-    assertTaskExists(this.webapp, 'html', [], done);
-  });
+    it('should contain images task', function(done) {
+        assertTaskExists(this.webapp, 'images', [], done);
+    });
 
-  it('should contain images task', function (done) {
-    assertTaskExists(this.webapp, 'images', [], done);
-  });
+    it('should contain fonts task', function(done) {
+        assertTaskExists(this.webapp, 'fonts', [], done);
+    });
 
-  it('should contain fonts task', function (done) {
-    assertTaskExists(this.webapp, 'fonts', [], done);
-  });
+    it('should contain clean task', function(done) {
+        assertTaskExists(this.webapp, 'clean', [], done);
+    });
 
-  it('should contain clean task', function (done) {
-    assertTaskExists(this.webapp, 'clean', [], done);
-  });
+    it('should contain build task', function(done) {
+        assertTaskExists(this.webapp, 'build', [], done);
+    });
 
-  it('should contain build task', function (done) {
-    assertTaskExists(this.webapp, 'build', [], done);
-  });
+    it('should contain connect task', function(done) {
+        assertTaskExists(this.webapp, 'connect', [], done);
+    });
 
-  it('should contain connect task', function (done) {
-    assertTaskExists(this.webapp, 'connect', [], done);
-  });
+    it('should contain watch task', function(done) {
+        assertTaskExists(this.webapp, 'watch', [], done);
+    });
 
-  it('should contain watch task', function (done) {
-    assertTaskExists(this.webapp, 'watch', [], done);
-  });
+    it('should contain default task', function(done) {
+        assertTaskExists(this.webapp, 'default', [], done);
+    });
 
-  it('should contain default task', function (done) {
-    assertTaskExists(this.webapp, 'default', [], done);
-  });
-
-  it('should contain default serve', function (done) {
-    assertTaskExists(this.webapp, 'serve', [], done);
-  });
+    it('should contain default serve', function(done) {
+        assertTaskExists(this.webapp, 'serve', [], done);
+    });
 });
--- test/test.js
+++ test/test.js
@@ -4,48 +4,46 @@
 var path = require('path');
 var helpers = require('yeoman-generator').test;
 
-describe('Gulp webapp generator test', function () {
-  beforeEach(function (done) {
-    helpers.testDirectory(path.join(__dirname, 'temp'), function (err) {
-      if (err) {
-        return done(err);
-      }
+describe('gulp-angular-semantic-ui generator test', function() {
+    beforeEach(function(done) {
+        helpers.testDirectory(path.join(__dirname, 'temp'), function(err) {
+            if (err) {
+                return done(err);
+            }
 
-      this.webapp = helpers.createGenerator('gulp-webapp:app', [
-        '../../app', [
-          helpers.createDummyGenerator(),
-          'mocha:app'
-        ]
-      ]);
-      this.webapp.options['skip-install'] = true;
+            this.webapp = helpers.createGenerator('gulp-angular-semantic-ui:app', [
+                '../../app', [
+                    helpers.createDummyGenerator(),
+                    'mocha:app'
+                ]
+            ]);
+            this.webapp.options['skip-install'] = true;
 
-      done();
-    }.bind(this));
-  });
-
-  it('the generator can be required without throwing', function () {
-    // not testing the actual run of generators yet
-    this.app = require('../app');
-  });
-
-  it('creates expected files', function (done) {
-    var expected = [
-      'bower.json',
-      'package.json',
-      'gulpfile.js',
-      'app/favicon.ico',
-      'app/robots.txt',
-      'app/index.html',
-      'app/scripts/main.js'
-    ];
-
-    helpers.mockPrompt(this.webapp, {
-      features: ['includeSass']
+            done();
+        }.bind(this));
     });
 
-    this.webapp.run({}, function () {
-      helpers.assertFile(expected);
-      done();
+    it('the generator can be required without throwing', function() {
+        // not testing the actual run of generators yet
+        this.app = require('../app');
     });
-  });
+
+    it('creates expected files', function(done) {
+        var expected = [
+            'bower.json',
+            'package.json',
+            'gulpfile.js',
+            'app/index.html',
+            'app/scripts/app.js',
+            'app/scripts/app-controller.js',
+            'app/scripts/app-service.js',
+            'app/scripts/app-directives.js',
+            'app/styles/main.css'
+        ];
+
+        this.webapp.run({}, function() {
+            helpers.assertFile(expected);
+            done();
+        });
+    });
 });
